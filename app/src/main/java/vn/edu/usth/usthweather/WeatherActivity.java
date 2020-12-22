dfbaee5d14a93fa98f80575e62099dac99c92a1f
Nathan <namluu253@gmail.com>
2020-12-22 15:01:34 +0700

    pushing practical 3

--- app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -10,21 +10,33 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(
+                R.id.container, firstFragment).commit();
     }
+
+    @Override
     public void onStart() {
 
         super.onStart();
+
     }
+
+    @Override
     public void onResume() {
 
         super.onResume();
     }
+
+    @Override
     public void onPause() {
         super.onPause();
     }
+    @Override
     public void onStop() {
         super.onStop();
     }
+    @Override
     public void onDestroy() {
         super.onDestroy();
     }
