34fc24f9916acfcedb03cb4510a64ad3aca8c281
Nathan <namluu253@gmail.com>
2020-12-22 14:20:35 +0700

    pushing practical 2

---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,31 @@
+package vn.edu.usth.usthweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+    public void onStart() {
+
+        super.onStart();
+    }
+    public void onResume() {
+
+        super.onResume();
+    }
+    public void onPause() {
+        super.onPause();
+    }
+    public void onStop() {
+        super.onStop();
+    }
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
